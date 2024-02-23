7d8dccc9b168fa7a200efe280466779aae178b6a
GeonHyeock <gunheock@gmail.com>
2024-02-23 15:00:49 +0000

    Feat : ray demo

--- streamlit_frontend/ray_infer.py
+++ streamlit_frontend/ray_infer.py
@@ -8,6 +8,7 @@ from stqdm import stqdm
 from util import draw_bbox_array, Infer, make_csv
 
 
+@ray.remote
 def batch_infer(
     img_path_list,
     img_shape=(640, 640),
@@ -28,15 +29,19 @@ def batch_infer(
                 csv_name = ".".join("/".join(file.split("/")[1:]).split(".")[:-1] + ["csv"])
                 csv_zip.writestr(csv_name, pd.DataFrame(csv).to_csv(index=False))
         csv_zip.extractall(f"inputdata2_result/conf{(conf_thres)}_iou{(iou_thres)}")
+    return
 
 
 if __name__ == "__main__":
+    ray.init()
     img_path_list = [
         root + "/" + file_name
         for root, _, files in os.walk("inputdata2")
         for file_name in files
         if os.path.splitext(file_name)[-1] in [".jpg", ".jpeg", ".png"]
-    ]
+    ][:11]
+    result = []
     for conf_thres in [0.1]:
         for iou_thres in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
-            batch_infer(img_path_list, conf_thres=conf_thres, iou_thres=iou_thres)
+            result.append(batch_infer.remote(img_path_list, conf_thres=conf_thres, iou_thres=iou_thres))
+    result
