baa0848bc845cece27d8cebd2fd95232210f074c
GeonHyeock <gunheock@gmail.com>
2024-02-27 11:37:04 +0000

    Feat : infer test

--- streamlit_frontend/ray_infer.py
+++ streamlit_frontend/ray_infer.py
@@ -6,6 +6,21 @@ import io
 import ray
 
 
+@ray.remote
+def batch_infer(img_path_list, conf_thres, iou_thres, img_shape=(640, 640), sic=False):
+    from streamlit_frontend import draw_bbox_array, Infer, make_csv
+
+    if not isinstance(img_path_list, list):
+        img_path_list = [img_path_list]
+    image = [cv2.imread(f) for f in img_path_list]
+    det = Infer(image, conf_thres, iou_thres)
+    det = draw_bbox_array(det, img_shape, image, sic, only_det=True)
+    csvs = make_csv(det)
+
+    csv_names = [".".join("/".join(file.split("/")[1:]).split(".")[:-1] + ["csv"]) for file in img_path_list]
+    return csvs, csv_names
+
+
 def Batch_Infer(
     img_path_list,
     batch_size=2,
@@ -19,34 +34,20 @@ def Batch_Infer(
         batch_infer.remote(img_path_list[idx : idx + batch_size], conf_thres, iou_thres, img_shape, sic)
         for idx in range(0, n, batch_size)
     ]
-    batch_result, buf = ray.get(batch_result), io.BytesIO()
+    buf = io.BytesIO()
     with zipfile.ZipFile(buf, "x") as csv_zip:
-        for csvs, csv_names in batch_result:
-            for csv, csv_name in zip(csvs, csv_names):
+        while len(batch_result):
+            done, batch_result = ray.wait(batch_result)
+            mini_batch_result = ray.get(done[0])
+            for csv, csv_name in zip(*mini_batch_result):
                 csv_zip.writestr(csv_name, pd.DataFrame(csv).to_csv(index=False))
         csv_zip.extractall(f"inputdata2_result/conf{(conf_thres)}_iou{(iou_thres)}")
-    return
-
-
-@ray.remote
-def batch_infer(img_path_list, conf_thres, iou_thres, img_shape=(640, 640), sic=False):
-    from streamlit_frontend import draw_bbox_array, Infer, make_csv
-
-    if not isinstance(img_path_list, list):
-        img_path_list = [img_path_list]
-    image = [cv2.imread(f) for f in img_path_list]
-    det = Infer(image, conf_thres, iou_thres)
-    det = draw_bbox_array(det, img_shape, image, sic, only_det=True)
-    csvs = make_csv(det)
-
-    csv_names = [
-        ".".join(("/".join(file.split("/")[1:]) + f"_conf{(conf_thres)}_iou{(iou_thres)}").split(".")[:-1] + ["csv"])
-        for file in img_path_list
-    ]
-    return csvs, csv_names
 
 
 if __name__ == "__main__":
+    import time
+    import numpy as np
+
     ray.init()
     img_path_list = [
         root + "/" + file_name
@@ -54,7 +55,14 @@ if __name__ == "__main__":
         for file_name in files
         if os.path.splitext(file_name)[-1] in [".jpg", ".jpeg", ".png"]
     ]
+
     result = []
-    for conf_thres in [0.1]:
-        for iou_thres in [0.1]:
-            Batch_Infer(img_path_list)
+    for conf_thres in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+        for iou_thres in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+            start = time.time()
+            Batch_Infer(img_path_list, conf_thres=conf_thres, iou_thres=iou_thres)
+            end = time.time()
+        result.append(end - start)
+    f = open("batch_infer_time.txt", "w")
+    f.write(np.mean(result))
+    f.close()
